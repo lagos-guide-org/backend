53962d29150eb17809c257db125110372bc3bc7f
Olaniyi John <olaniyijohnayomide@gmail.com>
2025-04-22 21:22:39 +0100

    added the testing data

---
+++ data.ts
@@ -0,0 +1,54 @@
+// {
+//     "name": "Ajah",
+//     "lat": 6.4698,
+//     "lng": 3.6021,
+//     "type": "bus_stop"
+//   }
+
+//   {
+//     "name": "Jakande",
+//     "lat": 6.4464,
+//     "lng": 3.5366,
+//     "type": "bus_stop"
+//   }
+
+//   {
+//     "name": "Lekki Phase 1",
+//     "lat": 6.4402,
+//     "lng": 3.4832,
+//     "type": "landmark"
+//   }
+
+//   {
+//     "name": "Lekki",
+//     "lat": 6.4281,
+//     "lng": 3.4216,
+//     "type": "bus_stop"
+//   }
+
+//   {
+//     "fromName": "Ajah",
+//     "toName": "Jakande",
+//     "distance": 4000,
+//     "duration": 10,
+//     "roadType": "express",
+//     "mode": "bus"
+//   }
+
+//   {
+//     "fromName": "Jakande",
+//     "toName": "Lekki Phase 1",
+//     "distance": 3500,
+//     "duration": 8,
+//     "roadType": "express",
+//     "mode": "bus"
+//   }
+
+//   {
+//     "fromName": "Lekki Phase 1",
+//     "toName": "Lekki",
+//     "distance": 3000,
+//     "duration": 6,
+//     "roadType": "street",
+//     "mode": "bus"
+//   }
